af8f2a3a2a2bf5ac37bae1efad4a74f5337dfbbc
unknown <ray1515123@gmail.com>
2022-02-05 17:31:35 +0800

    init

--- config.py
+++ config.py
@@ -13,9 +13,9 @@
 params = {
 	'input_size': 64, # Input size
     'batch_size': 1, # Batch size.
-    'epochs': 100000, # Number of epochs to train for.
+    'epochs': 5000, # Number of epochs to train for.
     'learning_rate': 0.00005, # Learning rate.
     'beta1': 0.9, # beta1 of Adam optimizer
     'beta2': 0.999, # beta2 of Adam optimizer
-    'save_epoch' : 1000, # Save checkpoint per save_epoch
+    'save_epoch' : 100, # Save checkpoint per save_epoch
     'save_path': 'C:\\Users\\User\\Desktop\\STNGAN\\result'} # Dir to save testing results
